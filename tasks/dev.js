10894c1b1e26d84e6e0a0bf46b5aae0f0bd40dcc
Tiankui <eric.prototype@gmail.com>
2013-02-21 21:07:15 +0800

    Update tasks/dev.js
    ++

--- tasks/dev.js
+++ tasks/dev.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-    return grunt.registerTask("dev", "runs linemans dev lifecycle tasks", function() {
+    return grunt.registerTask("dev", "runs bai dev tasks", function() {
         var appTasks;
         appTasks = require(process.cwd() + "/engine/config/application").appTasks;
         return grunt.task.run(appTasks.dev);
